41d2c5d60ebbc7b7a287b7775f3b40a201679537
marco-tav <marco.tavella@outlook.com>
2023-04-02 18:02:37 -0300

    Agregar ej3 y dudas del ej3

---
+++ tp1/parte1/ej3.pas
@@ -0,0 +1,28 @@
+(* Realizar un programa que lea 3 números enteros y los imprima en orden descendente.
+Por ejemplo, si se ingresan los valores 4, -10 y 12, deberá imprimir: 12 4 -10 *)
+
+program tp01-p1-ej03
+var
+  num, mayor, medio, menor: integer;
+begin
+  read(num);
+  mayor := num;
+  
+  read(num);
+  if (num <= mayor) then
+    medio := num
+  else
+    medio := mayor
+    mayor := num;
+  
+  read(num);
+  if (num <= medio) then
+    menor := num
+  else if ((num < mayor) and (num > medio)) then
+    menor := medio
+    medio := num
+  else 
+    menor := medio
+    medio := mayor
+    mayor := num;
+end;
