384730b82be9db84b0ce0ecb67373343154f035b
marco-tav <marco.tavella@outlook.com>
2023-04-08 20:30:42 -0300

    Agregar ej9 sin resolver

---
+++ tp1/parte1/ej9.pas
@@ -0,0 +1,4 @@
+(* Realizar un programa que lea un carácter, que puede ser “+” (suma) o “-” (resta); si se ingresa otro
+carácter, debe informar un error y finalizar. Una vez leído el carácter de suma o resta, deberá leerse
+una secuencia de números enteros que finaliza con 0. El programa deberá aplicar la operación leída
+con la secuencia de números, e imprimir el resultado final. *)
