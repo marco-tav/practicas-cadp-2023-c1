b20ababd025fe83be0aa7c6f73f9e8aee8d740f2
marco-tav <marco.tavella@outlook.com>
2023-04-09 13:12:27 -0300

    Agregar ej1 del tp1 parte 2

---
+++ tp1/parte2/ej1.pas
@@ -0,0 +1,18 @@
+(* Realizar un programa que lea 10 números enteros e informe la suma total de los números leídos *)
+
+program tp01-p2-ej1
+const
+  max_i = 10;
+var 
+  num, i, suma: integer;
+begin
+  suma := 0;
+
+  for i := 1 to 10 do 
+  begin
+    read(num);
+    suma := suma + num;
+  end;
+
+  writeln('La suma es ', suma, '.');
+end;
