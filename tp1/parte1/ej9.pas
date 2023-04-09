9b92715b5b73d98fce1058d0715df54ac2a8773a
marco-tav <marco.tavella@outlook.com>
2023-04-09 10:20:38 -0300

    Agregar ej9 resuelto y dudas

--- tp1/parte1/ej9.pas
+++ tp1/parte1/ej9.pas
@@ -2,3 +2,33 @@
 carácter, debe informar un error y finalizar. Una vez leído el carácter de suma o resta, deberá leerse
 una secuencia de números enteros que finaliza con 0. El programa deberá aplicar la operación leída
 con la secuencia de números, e imprimir el resultado final. *)
+
+program tp01-p1-ej09
+var 
+  operacion: char;
+  num, resultado: integer;
+begin 
+  write('Ingresá un más o un menos:');
+  read(operacion);
+  resultado := 0;
+
+  if ((operacion = '+') or (operacion = '-')) then begin
+    read(num);
+
+    if (operación = '+') then begin 
+      while(num <> 0) do begin
+        resultado := resultado + num;
+        read(num);
+      end;
+    else
+      while(num <> 0) do begin
+        resultado := resultado - num;
+        read(num);
+      end;
+    end;
+    writeln('El resultado es ', resultado, '.');
+
+  else
+    writeln('Solo se puede ingresar + o -!');
+  end;
+
