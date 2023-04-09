53025847dc2197e1cf763702a0b0b5279eca1f6d
marco-tav <marco.tavella@outlook.com>
2023-04-09 13:15:19 -0300

    Agregar ejercicio 1a del tp1, parte 2

---
+++ tp1/parte2/ej1a.pas
@@ -0,0 +1,22 @@
+(* Modifique el ejercicio 1 para que además informe la cantidad de números mayores a 5. *)
+
+program tp01-p2-ej1
+const
+  max_i = 10;
+var 
+  num, i, suma, Ma5: integer;
+begin
+  suma := 0;
+  Ma5 := 0;
+
+  for i := 1 to 10 do 
+  begin
+    read(num);
+    suma := suma + num;
+
+    if(num > 5) then Ma5 := Ma5 + 1;
+  end;
+
+  writeln('La suma es ', suma, '.');
+  writeln('Se ingresaron ', Ma5, 'números mayores a 5.');
+end;
